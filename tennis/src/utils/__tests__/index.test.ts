6fc1187bd3e6befcf6bf10e26cc97ee8b7ca86cb
Terence Huynh <me@terencehuynh.com>
2020-08-25 00:12:35 +1000

    Add more tests to cover hasWonGame

--- tennis/src/utils/__tests__/index.test.ts
+++ tennis/src/utils/__tests__/index.test.ts
@@ -92,5 +92,53 @@ describe("Utils", () => {
       const mode = Mode.NORMAL;
       expect(hasWonGame(mode, p1, p2)).toEqual(null);
     });
+    it("deuce game: player 1 has won the game point", () => {
+      const p1 = new Player("Player 1", { points: 5 });
+      const p2 = new Player("Player 2", { points: 3 });
+      const mode = Mode.DEUCE;
+      expect(hasWonGame(mode, p1, p2)).toEqual({ winner: p1.name });
+    });
+    it("deuce game: player 2 has won the game point", () => {
+      const p1 = new Player("Player 1", { points: 3 });
+      const p2 = new Player("Player 2", { points: 5 });
+      const mode = Mode.DEUCE;
+      expect(hasWonGame(mode, p1, p2)).toEqual({ winner: p2.name });
+    });
+    it("deuce game: in deuce", () => {
+      const p1 = new Player("Player 1", { points: 4 });
+      const p2 = new Player("Player 2", { points: 4 });
+      const mode = Mode.DEUCE;
+      expect(hasWonGame(mode, p1, p2)).toEqual(null);
+    });
+    it("deuce game: player has advantage", () => {
+      const p1 = new Player("Player 1", { points: 4 });
+      const p2 = new Player("Player 2", { points: 3 });
+      const mode = Mode.DEUCE;
+      expect(hasWonGame(mode, p1, p2)).toEqual(null);
+    });
+    it("tiebreak: player 1 wins", () => {
+      const p1 = new Player("Player 1", { points: 7 });
+      const p2 = new Player("Player 2", { points: 5 });
+      const mode = Mode.TIEBREAK;
+      expect(hasWonGame(mode, p1, p2)).toEqual({ winner: p1.name });
+    });
+    it("tiebreak: player 2 wins", () => {
+      const p1 = new Player("Player 1", { points: 7 });
+      const p2 = new Player("Player 2", { points: 9 });
+      const mode = Mode.TIEBREAK;
+      expect(hasWonGame(mode, p1, p2)).toEqual({ winner: p2.name });
+    });
+    it("tiebreak: does not reach minimum points", () => {
+      const p1 = new Player("Player 1", { points: 5 });
+      const p2 = new Player("Player 2", { points: 5 });
+      const mode = Mode.TIEBREAK;
+      expect(hasWonGame(mode, p1, p2)).toEqual(null);
+    });
+    it("tiebreak: does not reach margin", () => {
+      const p1 = new Player("Player 1", { points: 7 });
+      const p2 = new Player("Player 2", { points: 6 });
+      const mode = Mode.TIEBREAK;
+      expect(hasWonGame(mode, p1, p2)).toEqual(null);
+    });
   });
 });
