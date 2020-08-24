9e0e1b75199cd5534d70acfbd8ee7bd0451fda2c
Terence Huynh <me@terencehuynh.com>
2020-08-24 23:25:26 +1000

    Add initial match handling logic

---
+++ tennis/src/Match.ts
@@ -0,0 +1,120 @@
+import Player, { IPlayerState } from "./Player";
+import { Mode } from "./constants";
+import {
+  isDeuce,
+  isTiebreak,
+  hasAdvantage,
+  hasWonGame,
+  hasWonMatch,
+} from "./utils";
+
+interface IMatchState {
+  player1State?: IPlayerState;
+  player2State?: IPlayerState;
+  mode?: Mode;
+  completed?: boolean;
+}
+
+class Match {
+  private players: { [key: string]: Player };
+  private mode: Mode;
+  private completed: boolean;
+  private winner: string;
+
+  constructor(player1: string, player2: string, state?: IMatchState) {
+    this.players = {
+      [player1]: new Player(player1, state?.player1State),
+      [player2]: new Player(player2, state?.player2State),
+    };
+    this.mode = state?.mode ?? Mode.NORMAL;
+    this.completed = state?.completed ?? false;
+  }
+
+  /**
+   * Record player who won a point
+   * @param  player name of player
+   */
+  pointWonBy(player: string) {
+    // If the match has been won by someone, stop allowing this to
+    if (this.completed) return;
+
+    // Add point and recalculate state
+    this.players[player].addPoint();
+    this.afterAddPoint();
+  }
+
+  /**
+   * Display current state of the match in string format. In the case of deuce,
+   * instead of showing the points, it will show who has the advantage or when
+   * it is currently in deuce.
+   *
+   * Note: in a tiebreaker situation, the normal scoring system is changed to
+   * just showing the "raw" number of points (e.g. 1, 2, 3 vs 15, 30, 40).
+   */
+  output() {
+    const [p1, p2] = Object.values(this.players);
+    const completedText = this.completed ? `(${this.winner})` : "";
+    let points = "";
+
+    switch (this.mode) {
+      case Mode.DEUCE: {
+        const advantage = hasAdvantage(this.mode, p1, p2);
+        if (hasAdvantage) {
+          points = `Advantage ${advantage}`;
+        } else {
+          points = "Deuce";
+        }
+        break;
+      }
+      case Mode.TIEBREAK: {
+        points = `${p1.points}-${p2.points}`;
+        break;
+      }
+      default:
+        points = `${p1.tennisPoints}-${p2.tennisPoints}`;
+    }
+
+    return `${p1.games}-${p2.games}, ${points}${completedText}`;
+  }
+
+  /**
+   * After adding a point to a player, calculate the match's next state. It does
+   * the following:
+   *
+   * 1. Check if the game has been won by a player.
+   *    1a. If not, then we check if it is in a deuce state. We terminate from
+   *        here because the game is stil continuing.
+   *
+   * 2. If the game has been won by the player, add it to the winner's scoreboard
+   *
+   * 3. Check, by adding the game, that the overall match has been won.
+   *    3a. If the match has been won, we mark match as completed and terminate
+   *        from here since there is nothing to do.
+   *
+   * 4. If the match is still continuing, we check if there's a tiebreak match
+   *    before resetting the points to signifiy a new game is to be played.
+   */
+  private afterAddPoint() {
+    const [p1, p2] = Object.values(this.players);
+
+    // Have they won a game?
+    const wonGame = hasWonGame(this.mode, p1, p2);
+    if (!wonGame) {
+      if (isDeuce(this.mode, p1, p2)) this.mode = Mode.DEUCE;
+      return;
+    }
+    this.players[wonGame.winner].addGame();
+    const wonMatch = hasWonMatch(p1, p2);
+    if (wonMatch) {
+      this.winner = wonMatch.winner;
+      this.completed = true;
+      return;
+    }
+
+    if (isTiebreak(p1, p2)) this.mode = Mode.TIEBREAK;
+    p1.newGame();
+    p2.newGame();
+  }
+}
+
+export default Match;
